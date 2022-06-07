141daf3550bbd88146d11a4c0dccf88fa4a3b72a
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-07 23:10:31 +0300

    added sited for the admin panel

---
+++ instagramapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Image,Profile,Likes,Comments
+
+# Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Likes)
+admin.site.register(Comments)
